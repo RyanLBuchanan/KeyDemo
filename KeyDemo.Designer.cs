a29a31506c5f23fd3c2c6cd0504c8b7285406219
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-07-12 13:57:58 -0600

    Add project files.

---
+++ KeyDemo.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace KeyDemo
+{
+    partial class KeyDemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.charLabel = new System.Windows.Forms.Label();
+            this.keyInfoLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // charLabel
+            // 
+            this.charLabel.AutoSize = true;
+            this.charLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.charLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.charLabel.Location = new System.Drawing.Point(70, 42);
+            this.charLabel.MaximumSize = new System.Drawing.Size(150, 20);
+            this.charLabel.MinimumSize = new System.Drawing.Size(150, 20);
+            this.charLabel.Name = "charLabel";
+            this.charLabel.Size = new System.Drawing.Size(150, 20);
+            this.charLabel.TabIndex = 0;
+            //this.charLabel.Click += new System.EventHandler(this.charLabel_Click);
+            // 
+            // keyInfoLabel
+            // 
+            this.keyInfoLabel.AutoSize = true;
+            this.keyInfoLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.keyInfoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keyInfoLabel.Location = new System.Drawing.Point(70, 144);
+            this.keyInfoLabel.MaximumSize = new System.Drawing.Size(150, 150);
+            this.keyInfoLabel.MinimumSize = new System.Drawing.Size(150, 150);
+            this.keyInfoLabel.Name = "keyInfoLabel";
+            this.keyInfoLabel.Size = new System.Drawing.Size(150, 150);
+            this.keyInfoLabel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.keyInfoLabel);
+            this.Controls.Add(this.charLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label charLabel;
+        private System.Windows.Forms.Label keyInfoLabel;
+    }
+}
+
---
+++ KeyDemo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyDemo
+{
+    public partial class KeyDemo : Form
+    {
+        public KeyDemo()
+        {
+            InitializeComponent();
+        }
+
+        //private void charLabel_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        private void KeyDemo_KeyPressed(object sender, KeyPressEventArgs e)
+        {
+            charLabel.Text = $"Key pressed: {e.KeyChar}";
+        }
+
+        private void KeyDemo_KeyDown(object sender, KeyPressEventArgs e)
+        {
+            keyInfoLabel.Text =
+                $"Alt: {(e.Alt ? "Yes" : "No")}\n" +
+                $"Shift: {(e.Shift ? "Yes" : "No")}\n" +
+                $"Control: {(e.Control ? "Yes" : "No")}\n" +
+                $"KeyCode: {e.KeyCode}\n" +
+                $"KeyData: {e.KeyData}\n" +
+                $"KeyValue: {e.KeyValue}\n";
+        }
+
+        private void KeyDemo_KeyUp(object sender, KeyPressEventArgs e)
+        {
+            charLabel.Text = "";
+            keyInfoLabel.Text = "";
+        }
+    }
+}
